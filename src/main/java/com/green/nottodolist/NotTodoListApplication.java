03009dd8db9e786037b140208da6aba0f5a46702
남규진 <dft2007@naver.com>
2023-06-27 10:49:01 +0900

    start

---
+++ src/main/java/com/green/nottodolist/NotTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.green.nottodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotTodoListApplication.class, args);
+	}
+
+}
