22adc0813973ef8ef7f064a5c06f140e7efeee20
Haminsu <dldlma@naver.com>
2023-07-01 10:24:38 +0900

    test

--- src/main/java/com/green/nottodolist/NotTodoListApplication.java
+++ src/main/java/com/green/nottodolist/NotTodoListApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotTodoListApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(NotTodoListApplication.class, args);
 	}
