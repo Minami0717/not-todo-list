76dea613fb8431a09ba82f9bcea6d6d52e8fd976
정우진 <wash98@naver.com>
2023-07-06 13:14:36 +0900

    test

--- src/main/java/com/green/nottodolist/useList/UseListMapper.java
+++ src/main/java/com/green/nottodolist/useList/UseListMapper.java
@@ -2,7 +2,6 @@ package com.green.nottodolist.useList;
 
 import com.green.nottodolist.useList.model.UseListDailyVo;
 import com.green.nottodolist.useList.model.UseListInsDto;
-import com.green.nottodolist.useList.model.UseListMonthVo;
 import com.green.nottodolist.useList.model.UseListUpdDto;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -12,6 +11,5 @@ import java.util.List;
 public interface UseListMapper {
     int insUseList(List<UseListInsDto> dto);
     int updUseList(UseListUpdDto dto);
-    List<UseListMonthVo> selUseListByMonth(String yearMonth);
-    List<UseListDailyVo> selUseListByDay(String day);
+
 }
