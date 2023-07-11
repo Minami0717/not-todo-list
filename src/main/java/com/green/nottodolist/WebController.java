2c9ad84ee0bc4c714b59b6e8fe0a286a4f1cdd37
남규진 <dft2007@naver.com>
2023-07-11 15:04:00 +0900

    프론트 파일 추가

---
+++ src/main/java/com/green/nottodolist/WebController.java
@@ -0,0 +1,19 @@
+package com.green.nottodolist;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController implements ErrorController {
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+
+    @GetMapping("/error")
+    public String error() {
+        return "redirect:/";
+    }
+}
